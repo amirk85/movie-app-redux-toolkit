948c830f220fccdde8f6a6e81439d9638712b6f8
Amir <khan.ayyub@happiestminds.com>
2022-05-03 16:14:55 +0530

    .

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import MovieList from "../../Movies/MovieList";
-import SeriesList from "../../Shows/ShowList";
+import ShowList from "../../Shows/ShowList";
+import { useSelector } from "react-redux";
+import { MOVIES_DATA } from "../../../store/movie-slice";
 
 export default function Home() {
+  const movieData = useSelector(MOVIES_DATA);
+  console.log(movieData);
+
   return (
-    <div>
-      <MovieList />
-      <SeriesList />
-    </div>
+    <>
+      <main>
+        <MovieList />
+        <ShowList />
+      </main>
+    </>
   );
 }
